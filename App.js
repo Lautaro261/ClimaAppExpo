3c16d530b3df78ea6d1af71edac1c79f0e9c1974
Nahuel Lautaro Torres Loretto <112157731+Lautaro261@users.noreply.github.com>
2023-09-04 23:56:24 -0300

    migracion a Expo por fallos de dependencias

--- App.js
+++ App.js
@@ -1,20 +1,99 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import {  StyleSheet, ScrollView, View, Text, Keyboard, TouchableWithoutFeedback, Alert } from 'react-native';
+import Form from './src/components/Form';
+import Clima from './src/components/Clima';
 
-export default function App() {
+const App  = () => {
+
+  const [ busqueda, guardarBusqueda ] = useState({
+    ciudad: '',
+    pais: ''
+  });
+  const [consultar, guardarConsultar] = useState(false);
+  const [resultado, guardarResultado ] = useState({});
+  const [bgcolor, guardarBgcolor] = useState('rgb(71, 149, 212)');
+
+  const { ciudad, pais } = busqueda;
+
+  useEffect(() => {
+      const consultarClima = async () => {
+        if(consultar) {
+          const appId = '9cb2f3813782ee9492c2526b77572758';
+          const url = `http://api.openweathermap.org/data/2.5/weather?q=${ciudad},${pais}&appid=${appId}`;
+    
+          try {
+              const respuesta = await fetch(url);
+              const resultado = await respuesta.json();
+              guardarResultado(resultado);
+              guardarConsultar(false);
+
+              // Modifica los colores de fondo basado en la temperatura
+
+              const kelvin = 273.15;
+              const { main } = resultado;
+              const actual = main.temp - kelvin;
+
+              if(actual < 10) {
+                guardarBgcolor('rgb( 105, 108, 149 )');
+              } else if(actual >= 10 && actual < 25) {
+                guardarBgcolor('rgb(71, 149, 212)');
+              } else {
+                guardarBgcolor('rgb( 178, 28, 61)');
+              }
+              
+          } catch (error) {
+              mostrarAlerta();
+          }
+        }
+      }
+      consultarClima();
+  }, [consultar]);
+
+  const mostrarAlerta = () => {
+      Alert.alert(
+          'Error',
+          'No hay resultados, intenta con otra ciudad o país',
+          [{ text: 'OK '}]
+      )
+  }
+
+  const ocultarTeclado = () => {
+    Keyboard.dismiss();
+  }
+
+  const bgColorApp = {
+    backgroundColor: bgcolor
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+        <TouchableWithoutFeedback onPress={ () => ocultarTeclado() }>
+          <View style={[styles.app, bgColorApp ]}>
+              <View style={styles.contenido}>
+                <Clima
+                  resultado={resultado}
+                />
+                <Form 
+                  busqueda={busqueda}
+                  guardarBusqueda={guardarBusqueda}
+                  guardarConsultar={guardarConsultar}
+                />
+
+              </View>
+          </View>
+      </TouchableWithoutFeedback>
+    </>
   );
-}
+};
 
 const styles = StyleSheet.create({
-  container: {
+  app: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
   },
+  contenido: {
+    marginHorizontal: '2.5%'
+  }
 });
+
+export default App;
